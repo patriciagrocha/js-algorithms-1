8cf85285584d21eccea4c702562566bbef7f6485
Patricia Rocha <patirocha.dev@gmail.com>
2023-06-15 15:52:21 -0300

    fn color translation

--- exercicio2.js
+++ exercicio2.js
@@ -6,24 +6,26 @@
  * para fazer o mapeamento das cores.
  */
 
-const color = "green";
+const colorTranslation = (color) => {
+  switch (color) {
+    case "green":
+      console.log(`Tradução da cor ${color} é verde.`);
+      break;
+    case "black":
+      console.log(`Tradução da cor ${color} é preto.`);
+      break;
+    case "white":
+      console.log(`Tradução da cor ${color} é branco.`);
+      break;
+    case "blue":
+      console.log(`Tradução da cor ${color} é azul.`);
+      break;
+    case "gray":
+      console.log(`Tradução da cor ${color} é cinza.`);
+      break;
+    default:
+      console.log("Cor inválida!");
+  }
+};
+colorTranslation("blue");
 
-switch (color) {
-  case "green":
-    console.log(`Tradução da cor ${color} é verde.`);
-    break;
-  case "black":
-    console.log(`Tradução da cor ${color} é preto.`);
-    break;
-  case "white":
-    console.log(`Tradução da cor ${color} é branco.`);
-    break;
-  case "blue":
-    console.log(`Tradução da cor ${color} é azul.`);
-    break;
-  case "gray":
-    console.log(`Tradução da cor ${color} é cinza.`);
-    break;
-  default:
-    console.log('Cor inválida!');
-}
