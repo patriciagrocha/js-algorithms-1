8cde200a073d9e20d067c729c58bb16a94eb42df
Patricia Rocha <patirocha.dev@gmail.com>
2023-06-15 15:12:28 -0300

    color translation

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,25 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+const color = "green";
+
+switch (color) {
+  case "green":
+    console.log(`Tradução da cor ${color} é verde.`);
+    break;
+  case "black":
+    console.log(`Tradução da cor ${color} é preto.`);
+    break;
+  case "white":
+    console.log(`Tradução da cor ${color} é branco.`);
+    break;
+  case "blue":
+    console.log(`Tradução da cor ${color} é azul.`);
+    break;
+  case "gray":
+    console.log(`Tradução da cor ${color} é cinza.`);
+    break;
+  default:
+    console.log('Cor inválida!');
+}
