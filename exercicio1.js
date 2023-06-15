eb2deeede1685b1c9712f1d18a436fd4c7ecc4ec
Patricia Rocha <patirocha.dev@gmail.com>
2023-06-15 14:58:29 -0300

    age rating

--- exercicio1.js
+++ exercicio1.js
@@ -6,3 +6,17 @@
  * adulto (18-59 anos) ou idoso (60 anos ou mais). Utiliza uma estrutura condicional
  * (if, else if, else).
  */
+
+const age = 59;
+
+if(age >=0 && age <= 12) {
+    console.log('Faixa etária: criança');
+} else if (age > 12 && age <= 17) {
+    console.log('Faixa etária: adolescente');     
+} else if (age > 17 && age <= 59) {
+    console.log('Faixa etária: adulto'); 
+} else if (age >= 60) {
+    console.log('Faixa etária: idoso'); 
+} else {
+    console.log('Digite uma idade válida!'); 
+}
