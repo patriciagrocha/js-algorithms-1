6c5e6971d77ff2401e5b37c6270f8d8f71bd9c0a
Patricia Rocha <patirocha.dev@gmail.com>
2023-06-15 17:00:03 -0300

    feat: fn show words

--- exercicio5.js
+++ exercicio5.js
@@ -4,3 +4,22 @@
  * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
  * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
  */
+const words = ['programa', 'palavras','array', 'exibe','estrutura' ];
+
+const displayWords = (array) => {
+  array.forEach((word) => {
+    if (word.length == 5) {
+      console.log('cinco caracteres: ', word);
+    }
+  });
+};
+displayWords(words);
+
+function showWords(words) {
+  for (const word of words) {
+    if (word.length > 5) {
+       console.log(`mais de cinco caracteres: ${word}`);
+    }
+  }
+}
+showWords(words);
