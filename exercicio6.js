a0021bc01f7177d41c408b1083d8780d821a0f96
Patricia Rocha <patirocha.dev@gmail.com>
2023-06-19 16:33:59 -0300

    traversing array of objects

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,40 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+
+const pharmaProducts = [
+  { name: "Aspirina", price: 10.99 },
+  { name: "Paracetamol", price: 7.5 },
+  { name: "Antibiótico", price: 25.99 },
+  { name: "Vitamina C", price: 12.99 },
+  { name: "Analgésico", price: 8.75 },
+];
+
+// const displayProducts = (products) => {
+//   products.forEach((product) => {
+//     console.log(`Medicamento: ${product.name} - Preço: R$ ${product.price.toString().replace(".", ",")}`);
+//   });
+// };
+// displayProducts(pharmaProducts);
+
+const totalProducts = (products) => {
+  let qtdProducts = 0;
+  let valueTotal = 0;
+
+  products.forEach((product) => {
+    qtdProducts++;
+    valueTotal += product.price;
+
+    console.log(`\nMedicamento: ${product.name}`);
+    console.log(`Preço: R$ ${product.price.toString().replace(".", ",")}`);
+  });
+
+  console.log("\nQuantidade de produtos:", qtdProducts);
+  console.log("Valor total: R$", valueTotal.toFixed(2).toString().replace(".", ","));
+};
+totalProducts(pharmaProducts);
+
+
+
+
+
