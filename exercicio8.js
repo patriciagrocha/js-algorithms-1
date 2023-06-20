d53e81965ab7843015ee96c025f222c302068d71
Patricia Rocha <patirocha.dev@gmail.com>
2023-06-20 17:17:09 -0300

    calculator functions

--- exercicio8.js
+++ exercicio8.js
@@ -5,3 +5,33 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+
+
+const calculate = (num1, num2, fn) => {    
+    return fn(num1, num2); 
+}
+
+const sum = (a, b) => {
+    let result = a + b;
+    return result;
+}
+const sub = (a, b) => {
+    let result = a - b;
+    return result;
+}
+const mult = (a, b) => {
+    let result = a * b;
+    return result;
+}
+const div = (a, b) => {
+    let result = a / b;
+    return result.toFixed(2);
+}
+console.log(calculate(2,9,sum));
+console.log(calculate(2,4,sub));
+console.log(calculate(2,10,mult));
+console.log(calculate(2,8,div));
+
+
+
+
